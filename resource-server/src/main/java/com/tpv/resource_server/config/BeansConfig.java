5506cd4d584882bb264357766ef8f92cbc6b162e
FRC <pako.rc10.4@gmail.com>
2025-02-27 21:46:50 +0100

    ip privada local del auth y del front sustituye a localhost para poder acceder desde otros dispositivos distintos

--- resource-server/src/main/java/com/tpv/resource_server/config/BeansConfig.java
+++ resource-server/src/main/java/com/tpv/resource_server/config/BeansConfig.java
@@ -24,6 +24,7 @@ public class BeansConfig {
         cors.addAllowedMethod("*");
         cors.setAllowCredentials(true);
         cors.addAllowedOrigin("http://127.0.0.1:5173");
+        cors.addAllowedOrigin("http://192.168.1.42:5173");
         source.registerCorsConfiguration("/**", cors);
         return source;
     }
